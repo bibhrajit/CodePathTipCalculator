f6a2c3c344d04f01f19a9ee3dfe187bcc29ca4cc
Bibhrajit Halder <Bibhrajit@gmail.com>
2015-01-14 20:01:59 -0800

    Initial Commit

---
+++ Tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips
+//
+//  Created by Bibhrajit Halder on 1/14/15.
+//  Copyright (c) 2015 Bibhrajit Halder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
